bfc03bbb2b28bdfef711a5a81473dd80a93ebdeb
FackGoogle <38124127+FackGoogle@users.noreply.github.com>
2018-04-13 13:56:02 +0900

    Update MainActivity.java
    버튼추가

--- app/src/main/java/edu/android/gitproject/MainActivity.java
+++ app/src/main/java/edu/android/gitproject/MainActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Burron button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
         TextView textView = findViewById(R.id.FuckGoogle);
         TextView textView2 = findViewById(R.id.FuckGoogle);
     }
